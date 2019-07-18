5dcfa385ab56800be674431308200f7c642fa23b
Talal Ashraf <talal@pixleeteam.com>
2019-07-18 18:34:00 -0400

    Use index

--- main.tf
+++ main.tf
@@ -12,10 +12,10 @@ resource "aws_acm_certificate" "default" {
 resource "aws_route53_record" "validation" {
   count    = "${length(var.subject_alternative_names) + 1}"
 
-  name            = "${aws_acm_certificate.default.domain_validation_options.${count.index}.resource_record_name}"
-  type            = "${aws_acm_certificate.default.domain_validation_options.${count.index}.resource_record_type}"
+  name            = "${aws_acm_certificate.default.domain_validation_options[count.index].resource_record_name}"
+  type            = "${aws_acm_certificate.default.domain_validation_options[count.index].resource_record_type}"
   zone_id         = "${var.hosted_zone_id}"
-  records         = ["${aws_acm_certificate.default.domain_validation_options.${count.index}.resource_record_value}"]
+  records         = ["${aws_acm_certificate.default.domain_validation_options[count.index].resource_record_value}"]
   ttl             = "${var.validation_record_ttl}"
   allow_overwrite = "${var.allow_validation_record_overwrite}"
 }
